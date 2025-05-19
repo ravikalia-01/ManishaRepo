46d870d9b858c6e837bd92eae6d7181d587591ac
Ravi Kalia <kravi014@gmail.com>
2025-05-19 19:16:07 -0400

    screenshot in assets added

--- src/first.jsx
+++ src/first.jsx
@@ -5,9 +5,9 @@ function First() {
   return (
     <div class="mainFirst">
       <h1 class="headFirst">WelCome to React app.</h1>
-      <h2 class="head2">Assignment-1</h2>
+      <h2 class="head1">Best Indian Food Recipe</h2>
       <div class="bio">
-        <h2 class="head2">Information of next project in brief.</h2>
+        <h2 class="head2">South Indian</h2>
         <div class="picFlex">
           
           <img src="https://images.unsplash.com/photo-1589778655375-3e622a9fc91c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
@@ -15,6 +15,39 @@ function First() {
           <img src="https://images.unsplash.com/photo-1727404679933-99daa2a7573a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
           <img src="https://images.unsplash.com/photo-1690951784638-1f9039d74a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
         </div>
+        <h2 class="head2">North Indian</h2>
+        <div class="picFlex">
+          
+          <img src="https://images.unsplash.com/photo-1589778655375-3e622a9fc91c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1672477179695-7276b0602fa9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1727404679933-99daa2a7573a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1690951784638-1f9039d74a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+        </div>
+        <h2 class="head2">Mughlai</h2>
+        <div class="picFlex">
+          
+          <img src="https://images.unsplash.com/photo-1589778655375-3e622a9fc91c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1672477179695-7276b0602fa9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1727404679933-99daa2a7573a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1690951784638-1f9039d74a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+        </div>
+        <h2 class="head2">Punjabi</h2>
+        <div class="picFlex">
+          
+          <img src="https://images.unsplash.com/photo-1589778655375-3e622a9fc91c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1672477179695-7276b0602fa9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1727404679933-99daa2a7573a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1690951784638-1f9039d74a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+        </div>
+        <h2 class="head2">Indo-Chinese</h2>
+        <div class="picFlex">
+          
+          <img src="https://images.unsplash.com/photo-1589778655375-3e622a9fc91c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1672477179695-7276b0602fa9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1727404679933-99daa2a7573a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+          <img src="https://images.unsplash.com/photo-1690951784638-1f9039d74a6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xhc3NpYyUyMGluZGlhbiUyMGZvb2QlMjBwaWNzfGVufDB8fDB8fHww" alt="food india" class="pic1" />
+        </div>
+
       </div>
         <p class="para1">Hi,I am Manisha Sharma and this is WDDM_132, Week_1 assignment. in project_1, I want to create a cooking app because I love cooking. The app i want to create is basically a Recipe app that provides the recipe and indegrient to user for cooking their favourite dish/cousine. </p>
     </div>
